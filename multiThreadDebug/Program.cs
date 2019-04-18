0277cc43ade23717e63524a39e0178046fd0d4c2
Ищенко Вячеслав <ishchenkoviacheslav@live.com>
2019-04-18 12:49:36 +0300

    for/foreach tests added

--- multiThreadDebug/Program.cs
+++ multiThreadDebug/Program.cs
@@ -8,6 +8,14 @@ using System.Threading.Tasks;
 
 namespace multiThreadDebug
 {
+    class MyClass
+    {
+        public int MyProperty { get; set; }
+        public MyClass()
+        {
+            MyProperty = 4;
+        }
+    }
     class Program
     {
         private static readonly object loker = new object();
@@ -16,6 +24,41 @@ namespace multiThreadDebug
 
         static void Main(string[] args)
         {
+            System.Timers.Timer tim = new System.Timers.Timer() { Interval = 5000, Enabled = true, AutoReset = false };
+            tim.Elapsed += (object sender, System.Timers.ElapsedEventArgs e)=> {((System.Timers.Timer)sender).Enabled = false; };
+            while (true)
+            {
+                Thread.Sleep(1000);
+                if(!tim.Enabled)
+                {
+                    Console.WriteLine("Timer worked!");
+                    tim.Enabled = true;
+                }
+            }
+            //TimeSpan res = TimeSpan.Zero;
+            //DateTime tm;
+            //List<MyClass> l = new List<MyClass>();
+            //Random r2 = new Random();
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //    l.Add(new MyClass());
+            //}
+            //tm = DateTime.UtcNow;
+            //foreach (MyClass nu in l)
+            //{
+            //    nu.MyProperty = 4;
+            //}
+            //res = DateTime.UtcNow.Subtract(tm);
+            //Console.WriteLine(res);
+            //tm = DateTime.UtcNow;
+            //it's faster! On Read about 2 times, on write about 10 - 25%
+            //for (int rg = 0; rg < l.Count; rg++)
+            //{
+            //    l[rg].MyProperty = 4;
+            //}
+            //res = DateTime.UtcNow.Subtract(tm);
+            //Console.WriteLine(res);
+            Thread.Sleep(1000000000);
             //initialization
             Random r = new Random();
             for (int i = 0; i < 2000; i++)
@@ -45,37 +88,39 @@ namespace multiThreadDebug
                     }
                 }).Start();
             }
-            Thread.Sleep(3000);
             for (int t = 0; t < 50; t++)
             {
-                Thread.Sleep(10);
                 DateTime temp;
                 TimeSpan[] result = new TimeSpan[2000];
                 IPEndPoint ipAndPort = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
                 //2000
+                temp = DateTime.UtcNow;
+
                 for (int y = 0; y < MyList.Count; y++)
                 {
-                    temp = DateTime.UtcNow;
+                    //not 2000 but less
                     //if ((y % 10) == 0)
-                    // {
-                    lock (loker)
-                    {
-                        byte[] bytes = Encoding.ASCII.GetBytes("325bp945nbpoidyushdp875908mqeyrgiuqp57jvq84ytp8 eygt0-3 745-v89uqemroti nqpty some long long data..........." + MyList[y]);
-                        udp.Send(bytes, bytes.Length, ipAndPort);
-                    }
-                    //  }
-                    result[y] = DateTime.UtcNow.Subtract(temp);
+                    //{
+                        lock (loker)
+                        {
+                            byte[] bytes = Encoding.ASCII.GetBytes("325bp945nbpoidyushdp875908mqeyrgiuqp57jvq84ytp8 eygt0-3 745-v89uqemroti nqpty some long long data..........." + MyList[y]);
+                            udp.Send(bytes, bytes.Length, ipAndPort);
+                        }
+                    //}
+                    //result[y] = DateTime.UtcNow.Subtract(temp);
                 }
-                //for (int u = 0; u < result.Length; u++)
+                TimeSpan total = DateTime.UtcNow.Subtract(temp);
+                //TimeSpan total = TimeSpan.Zero;
+                //foreach (TimeSpan time in result)
                 //{
-                //    Console.WriteLine(result[u]);
+                //    total = total + time;
                 //}
-                TimeSpan total = TimeSpan.Zero;
-                foreach (TimeSpan time in result)
-                {
-                    total = total + time;
-                }
                 Console.WriteLine($"total: {total}");
+                //if total time of work less than 10 ms - make sleep
+                if (total < new TimeSpan(0,0,0,0,10))
+                {
+                    Thread.Sleep(10);
+                }
             }
 
             Console.WriteLine("Any key...");
